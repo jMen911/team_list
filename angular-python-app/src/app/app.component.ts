b9aa84f8f4fc697916b0a8c1700269bc6929aa3d
jMen911 <julien.mengeot@gmail.com>
2024-08-02 16:45:21 +0200

    Update ts files following tutorial

--- angular-python-app/src/app/app.component.ts
+++ angular-python-app/src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiserviceService } from './service';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-python-app';
+export class AppComponent implements OnInit {
+  newdata:any;
+
+  constructor(private _apiservice:ApiserviceService) { }
+
+  ngOnInit() {
+	this.getData();
+  }
+
+  getData() {
+	this._apiservice.getdata().subscribe(res=>{
+  	this.newdata=res;
+	})
+  }
 }
